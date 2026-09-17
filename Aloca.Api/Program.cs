6d146dc9f87c038e439aa7b34a451d464c4f52a2
xavierr-max <maxwelldearaujo1017@gmail.com>
2026-09-17 17:02:17 -0300

    fix: apply database migrations on startup

--- Aloca.Api/Program.cs
+++ Aloca.Api/Program.cs
@@ -25,6 +25,12 @@ builder.Services.AddScoped<FinancialSettingsService>();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AlocaDbContext>();
+    dbContext.Database.Migrate();
+}
+
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
